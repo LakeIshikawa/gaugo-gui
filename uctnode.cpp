45a6bae517eeea8231f2b3937538141ca83867b8
LakeIshikawa <lakeishikawa@gmail.com>
2014-05-09 13:05:00 +0900

    - UCT node stats parse bugfix

--- uctnode.cpp
+++ uctnode.cpp
@@ -5,7 +5,7 @@ UCTNode::UCTNode(QPoint pos, int wb, int p, int amafwb, int amafp, float uct)
     this->pos = pos;
     this->winsBlack = wb;
     this->played = p;
-    this->AMAFplayed = amafwb;
+    this->AMAFwinsBlack = amafwb;
     this->AMAFplayed = amafp;
     this->uctValue = uct;
 }
